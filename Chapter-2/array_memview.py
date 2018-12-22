8a2216130790d96a031b92bdc9d1bd4d04242309
Rishant Dutt <rishant.dutt96@gmail.com>
2018-12-22 12:13:24 +0530

    Chapter 2, complete.

--- Chapter-2/array_memview.py
+++ Chapter-2/array_memview.py
@@ -11,14 +11,31 @@ from random import random
    such as, .frombytes and .tofile
 """
 
-# floats = array('d', (random() for i in range(10 ** 7)))
-# print(floats[-1])
-# fp = open('floats.bin', 'wb')
-# floats.tofile(fp)
-# fp.close()
-# floats2 = array('d')
-# fp = open('floats.bin', 'rb')
-# floats2.fromfile(fp, 10 ** 7)
-# fp.close()
-# print(floats2[-1])
-# print(floats2 == floats)
+floats = array('d', (random() for i in range(10 ** 7)))
+print(floats[-1])
+fp = open('floats.bin', 'wb')
+floats.tofile(fp)
+fp.close()
+floats2 = array('d')
+fp = open('floats.bin', 'rb')
+floats2.fromfile(fp, 10 ** 7)
+fp.close()
+print(floats2[-1])
+print(floats2 == floats)
+
+"""
+ Memory Views -
+    the built-in 'memoryview' class is a shared memory
+    sequence type that lets you handle slices of array
+    without copying bytes.
+"""
+
+# Ex. Changing the value of an array item by poking one of its bytes.
+numbers = array('h', [-2, -1, 0, 1, 2])
+memv = memoryview(numbers)
+print(len(memv))
+print(memv[0])
+memv_oct = memv.cast('B')
+print(memv_oct.tolist())
+memv_oct[5] = 4
+print(numbers)
